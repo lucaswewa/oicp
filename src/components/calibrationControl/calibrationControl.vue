e190b1c783eb37bfebb670d256fb958b4367ea69
Lucas Wang <lucas.wang@gradientsensing.com>
2024-08-21 22:29:50 -0700

    Dark Field Calibration Panel

--- src/components/calibrationControl/calibrationControl.vue
+++ src/components/calibrationControl/calibrationControl.vue
@@ -1,11 +1,187 @@
-<script setup>
-import CalibrationControlPanel from './calibrationControlPanel.vue';
-</script>
-
 <template>
   <div uk-grid class="uk-height-1-1 uk-margin-remove uk-padding-remove">
-		<div class="control-component">
-			<CalibrationControlPanel />
-		</div>
+		<div class="settings-nav">
+      <ul class="uk-nav uk-nav-default">
+        <li class="uk-nav-header">Calibration Cat1</li>
+        <li>
+          <TabIcon
+            id="settings-display-icon"
+            tabID="Dark Field Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="Distortion Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+
+        <li>
+          <TabIcon
+            id="settings-features-icon"
+            tabID="Flat Field Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab">
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-camera-icon"
+            tabID="Color Shift Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li class="uk-nav-header">Calibration Cat2</li>
+        <li>
+          <TabIcon
+            id="settings-stage-icon"
+            tabID="Multi-Point Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="Stray-Light Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="Image Scaling Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="Single Filter Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="One Color Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="Four Color Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+        <li>
+          <TabIcon
+            id="settings-general-icon"
+            tabID="Brightness Rescaling Calibration"
+            :currentTab="currentTab"
+            @setTab="setTab"
+          >
+            <i class="material-icons">photo_camera</i>
+          </TabIcon>
+        </li>
+
+      </ul>
+    </div>
+
+    <div class="view-component uk-width-expand uk-padding-small">
+      <TabContent
+        tabID="Dark Field Calibration"
+        :currentTab="currentTab"
+      >
+        <div class="settings-pane uk-padding-small">
+          <DarkFieldCalibrationControl/>
+        </div>
+      </TabContent>
+    </div>
 	</div>
 </template>
+
+<script>
+import CalibrationControlPanel from './calibrationControlPanel.vue';
+import TabContent from '../tabControl/TabContent.vue';
+import TabIcon from '../tabControl/TabIcon.vue';
+
+import DarkFieldCalibrationControl from './darkFieldCalibrationControl.vue';
+
+export default {
+  name: "Calibration Content",
+
+  components: {
+    TabIcon,
+    TabContent,
+    CalibrationControlPanel,
+    DarkFieldCalibrationControl
+  },
+
+  data: function() {
+    return {
+      selected: "display",
+      currentTab: "display"
+    }
+  },
+
+  methods: {
+    setTab: function(event, tab) {
+      if (!(this.currentTab == tab)) {
+        this.currentTab = tab
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+// Custom UIkit CSS modifications
+@import "../../assets/less/theme.less";
+
+.settings-nav {
+  overflow-y: auto;
+  overflow-x: hidden;
+  width: 250px;
+  padding: 10px;
+  background-color: rgba(180, 180, 180, 0.03);
+  border-width: 0 1px 0 0;
+  border-style: solid;
+  border-color: rgba(180, 180, 180, 0.25);
+}
+
+.settings-nav li > a {
+  padding-left: 6px !important;
+  border-radius: @button-border-radius;
+}
+</style>
---
+++ src/components/calibrationControl/darkFieldCalibrationControl.vue
@@ -0,0 +1,123 @@
+<template>
+  <div
+    id="dark-control-panel"
+    class="uk-padding-small"
+  >
+    <ul uk-accordion="multiple: true">
+      <li :class="{ 'uk-open': true }">
+        <a class="uk-accordion-title" href="#">Dark Field Calibration Detail</a>
+        <p>[name], [display name]</p>
+        <p>description</p>
+        <p>binning_x</p>
+        <p>binning_y</p>
+        <p>image_width</p>
+        <p>image_height</p>
+        <p>image_data view</p>
+        <p>[ROI location], [ROI size], [ROI shape]</p>
+        <p>chart</p>
+        <p>[take image], [load image], [save], [delete], [next], [prev], [last], [first]</p>
+      </li>
+      <li :class="{ 'uk-open': true }">
+        <a class="uk-accordion-title" href="#">Dark Field Calibration List (6)</a>
+        <div class="uk-accordion-content">
+          <div>
+            <div class="uk-overflow-auto">
+              <table class="uk-table uk-table-hover uk-table-middle uk-table-divider">
+                <thead>
+                  <tr>
+                    <th class="uk-table-shrink"></th>
+                    <th class="uk-table-shrink">Preserve</th>
+                    <th class="uk-table-expand">Expand + Link</th>
+                    <th class="uk-width-small">Truncate</th>
+                    <th class="uk-table-shrink uk-text-nowrap">Shrink + Nowrap</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
+                    <td>
+                      <img class="uk-preserve-width uk-border-circle" src="../asserts/dark_field_cali.png" width="40" height="40" alt="">
+                    </td>
+                    <td class="uk-table-link">
+                      <a class="uk-link-reset" href="#" @click="clickMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+                  <tr>
+                    <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
+                    <td>
+                      <img class="uk-preserve-width uk-border-circle" src="../asserts/dark_field_cali.png" width="40" height="40" alt="">
+                    </td>
+                    <td class="uk-table-link">
+                      <a class="uk-link-reset" href="#" @click="clickMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+                  <tr>
+                    <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
+                    <td>
+                      <img class="uk-preserve-width uk-border-circle" src="../asserts/dark_field_cali.png" width="40" height="40" alt="">
+                    </td>
+                    <td class="uk-table-link">
+                      <a class="uk-link-reset" href="#" @click="clickMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+                  <tr>
+                    <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
+                    <td>
+                      <img class="uk-preserve-width uk-border-circle" src="../asserts/dark_field_cali.png" width="40" height="40" alt="">
+                    </td>
+                    <td class="uk-table-link">
+                      <a class="uk-link-reset" href="#" @click="clickMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+
+                  <tr>
+                    <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
+                    <td>
+                      <img class="uk-preserve-width uk-border-circle" src="../asserts/dark_field_cali.png" width="40" height="40" alt="">
+                    </td>
+                    <td class="uk-table-link">
+                      <a class="uk-link-reset" href="#" @click="clickMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+                  <tr>
+                    <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
+                    <td>
+                      <img class="uk-preserve-width uk-border-circle" src="../asserts/dark_field_cali.png" width="40" height="40" alt="">
+                    </td>
+                    <td class="uk-table-link">
+                      <a class="uk-link-reset" href="#" @click="clickMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DarkFieldCali",
+
+  methods: {
+    clickMe(event) {
+      console.log("clicked a dark field calibration item.")
+    }
+  }
+}
+</script>
